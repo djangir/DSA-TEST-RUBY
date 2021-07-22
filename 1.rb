f9a18781f2dab544a27897edf3a232322269a5da
Dinesh Jangir <87252360+djangir@users.noreply.github.com>
2021-07-22 17:05:00 +0530

    Update 1.rb

--- 1.rb
+++ 1.rb
@@ -1,9 +1,11 @@
 # 1. Create a function that subtracts one positive integer from another, without using any arithmetic operators such as -, %, /, +, etc.
 
 class Subb
+    
     def sub(a , b)
         Array.new.fill("a" , (b...a)).join("").length
     end  
 end  
+
 puts Subb.new.sub(1000 , 500)
  
